d8d1138e828d623730ce99e1221e4abf8bd9cad4
Kinga Zubrzycka <kinga.zubrzycka@gmail.com>
2020-09-27 12:33:37 +0200

    Addedd Add method

---
+++ ConsoleApp2/ConsoleApp2/FeatureA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class FeatureA
+    {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 1;
+            int sum = x1 + x2;
+            return sum;
+
+        }
+    }
+    
+}
